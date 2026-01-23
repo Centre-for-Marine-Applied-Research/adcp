837b43e8cb535d2c5d1a57f44e8c3a8fdc3363c5
Danielle Dempsey <ddempsey@perennia.ca>
2026-01-23 16:30:42 -0400

    change to 16 directions instead of 8

--- R/adcp_label_direction.R
+++ R/adcp_label_direction.R
@@ -5,10 +5,16 @@
 #' Likely more efficient in the original vectorized form. Modified to match
 #' \code{adcp_label_speed()}. Could re-visit both.
 #'
-#' Assigns direction data in bins of 45 degree C. -22.5 to 22.5 degree is North,
-#' 22.5 to 67.5 is NNE, etc.
+#' If \code{n_petals} is 8: Assigns direction data in bins of 45 degrees. -22.5
+#' to 22.5 degree is North, 22.5 to 67.5 is NE, etc.
+#'
+#' If \code{n_petals} is 16 (the default): Assigns direction data in bins of
+#' 22.5 degrees. -11.25 to 11.25 degree is North, 11.25 to 33.75 is NNE, etc.
 #'
 #' @param dat Data frame with column sea_water_to_direction_degree.
+#'
+#' @param n_petals Number of bins to divide direction data into. Must be either
+#'   8 or 16.
 #'
 #' @returns Returns \code{dat} with an additional column
 #'   \code{sea_water_to_direction_degree_labels}, the direction labels as an
@@ -17,21 +23,41 @@
 #' @export
 
 
-adcp_label_direction <- function(dat) {
+adcp_label_direction <- function(dat, n_petals = 16) {
 
-  dat$sea_water_to_direction_degree_labels <- cut(
-    dat$sea_water_to_direction_degree,
-    breaks = seq(22.5, 382.5, 45),
-    labels = c("NE", "E", "SE", "S", "SW", "W", "NW", "N")
-  )
+  if(n_petals != 8 & n_petals != 16) {
 
-  levels <- c("N", "NE", "E", "SE", "S", "SW", "W", "NW")
+    warning("n_petals must be 8 or 16, not << ", n_petals, ">>. n_petal will be changed to 16.")
 
-  # dat[
-  #   is.na(dat$sea_water_to_direction_degree_labels),
-  #   "sea_water_to_direction_degree_labels"
-  # ] <- "N"
+    n_petals <- 16
+  }
 
+  if(n_petals == 8) {
+    min_break <- 22.5
+
+    dat$sea_water_to_direction_degree_labels <- cut(
+      dat$sea_water_to_direction_degree,
+      breaks = seq(22.5, 382.5, 45),
+      labels = c("NE", "E", "SE", "S", "SW", "W", "NW", "N")
+    )
+
+    levels <- c("N", "NE", "E", "SE", "S", "SW", "W", "NW")
+  }
+
+  if(n_petals == 16) {
+    min_break <- 11.25
+
+    dat$sea_water_to_direction_degree_labels <- cut(
+      dat$sea_water_to_direction_degree,
+      breaks = seq(11.25, 371.25, 22.5),
+      labels = c(
+        "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S",
+        "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW", "N")
+    )
+
+    levels <- c("N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S",
+                "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW")
+  }
 
   dat %>%
     mutate(
@@ -39,7 +65,7 @@ adcp_label_direction <- function(dat) {
       sea_water_to_direction_degree_labels = if_else(
         is.na(sea_water_to_direction_degree_labels) &
           sea_water_to_direction_degree >= 0 &
-          sea_water_to_direction_degree <= 22.5, "N",
+          sea_water_to_direction_degree <= min_break, "N",
         sea_water_to_direction_degree_labels
       ),
       sea_water_to_direction_degree_labels =
